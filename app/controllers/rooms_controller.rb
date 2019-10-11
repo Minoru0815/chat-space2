eb716c6f0cebbfcf27d946a0be5aa1ab0bbd2466
Minoru0815 <bluexjapan6969@gmail.com>
2019-10-11 17:29:30 +0900

    create_group

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,9 @@
 class RoomsController < ApplicationController
-  def show
+  def index
+    if @group.index(group)
+      redirect_to group_rooms_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 end
